4819332589f44fdef86dc641b54d81cef12de363
Mayank Mishra <mayank71104@gmail.com>
2024-03-15 21:18:10 +0530

    vid1

--- SalesandRetail/SalesandRetail/settings.py
+++ SalesandRetail/SalesandRetail/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'inventory.apps.InventoryConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
