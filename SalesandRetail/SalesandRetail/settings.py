3839ded6c0d5b797b68d6f0566b07be892dbb0e6
Mayank Mishra <mayank71104@gmail.com>
2024-03-16 18:32:07 +0530

    login,signup,logout,navbar

--- SalesandRetail/SalesandRetail/settings.py
+++ SalesandRetail/SalesandRetail/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'users',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -54,7 +55,7 @@ ROOT_URLCONF = 'SalesandRetail.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
--- SalesandRetail/SalesandRetail/urls.py
+++ SalesandRetail/SalesandRetail/urls.py
@@ -16,7 +16,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from users import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('',views.LoginPage,name='login'),
+    path('signup/',views.SignupPage,name='signup'),
+    path('dashboard/',views.HomePage,name='dashboard'),
+    path('logout/',views.LogoutPage,name ='logout'),
 ]
