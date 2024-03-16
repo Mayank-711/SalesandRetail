2f97fd37bdc249d3c00c191d49214c103efa2334
Mayank Mishra <mayank71104@gmail.com>
2024-03-17 02:24:11 +0530

    minus

--- SalesandRetail/inventory/views.py
+++ SalesandRetail/inventory/views.py
@@ -3,6 +3,7 @@ from django.contrib.auth.decorators import login_required
 from django.contrib.auth import authenticate
 from .models import *
 from django.http import JsonResponse
+from django.db.models import F
 # Create your views here. 
 
 
@@ -32,5 +33,21 @@ def SalesPage(request):
     producttype = Inventory.objects.filter(username=username).values('P_Type').distinct()
     btype = Inventory.objects.filter(username=username).values('P_Brand').distinct()
     ntype = Inventory.objects.filter(username=username).values('p_Name').distinct()
-    context = {'ptypes':producttype,'brandtypes':btype,'pnames':ntype}
+    if request.method=="POST":
+        data=request.POST
+        customer_name=data.get('cname')
+        customer_email=data.get('cmail')
+        PS_Type=data.get("stype")
+        PS_Name=data.get("sname")
+        PS_Brand=data.get("sbrand")
+        QuantitySold=data.get("scount")
+        PS_date=data.get("sellingDate")
+        SellingPrice=data.get("sellingPrice")
+        Sales.objects.create(username=username,PS_Type=PS_Type,PS_Name=PS_Name,PS_Brand=PS_Brand,QuantitySold=QuantitySold,PS_Date=PS_date,SellingPrice=SellingPrice,customer_name=customer_name,customer_email=customer_email) 
+        Inventory.objects.filter(username=username, P_Type=PS_Type, P_Brand=PS_Brand, p_Name=PS_Name).update(
+        P_Stock=F('P_Stock') - int(QuantitySold)
+        )
+        return redirect("sales")
+    qset  = Sales.objects.all()
+    context = {'ptypes':producttype,'brandtypes':btype,'pnames':ntype,"Sales":qset}
     return render(request,'inventory/sales.html',context=context)
