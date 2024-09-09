78b700013d0450ed34839de4fcd553990a1f2984
Mayank Mishra <mayank71104@gmail.com>
2024-09-09 12:42:46 +0530

    build9

--- SalesandRetail/settings.py
+++ SalesandRetail/settings.py
@@ -111,7 +111,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # https://docs.djangoproject.com/en/5.0/topics/i18n/
 
 
-MEDIA_ROOT=(BASE_DIR / 'media')
+# MEDIA_ROOT=(BASE_DIR / 'media')
 
 
 MEDIA_URL='/media/'
@@ -130,6 +130,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR/'media'
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
