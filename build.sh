bfe4db7bccd0d76bce06bcaaa2fb4a417ab3da36
Mayank Mishra <mayank71104@gmail.com>
2024-09-09 12:17:22 +0530

    abcd

--- build.sh
+++ build.sh
@@ -1,4 +1,6 @@
 set -o errexit
 pip install -r requirements.txt
+pip install --upgrade pip
+pip install --force-reinstall -U setuptools
 python manage.py collectstatic --no-input
 python manage.py migrate
