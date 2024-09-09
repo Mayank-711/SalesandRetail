5e3513f7acd48f71bf548c9cfcc90d00c85f9256
Mayank Mishra <mayank71104@gmail.com>
2024-09-09 12:41:13 +0530

    build8

--- SalesandRetail/settings.py
+++ SalesandRetail/settings.py
@@ -130,7 +130,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_ROOT = BASE_DIR/'media'
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
