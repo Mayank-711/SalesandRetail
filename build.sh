768d86655e8459bb9c62a45256dce0bb638c1652
Mayank Mishra <mayank71104@gmail.com>
2024-09-09 12:27:41 +0530

    build4

--- build.sh
+++ build.sh
@@ -2,8 +2,8 @@ set -o errexit
 pip install -r requirements.txt
 pip install --upgrade pip
 pip install --force-reinstall -U setuptools
-python
-import django
-exit()
+# python
+# import django
+# exit()
 python manage.py collectstatic --no-input
 python manage.py migrate
