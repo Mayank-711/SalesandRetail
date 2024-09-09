937461906eca1619972ec05b8313b3722c5d8029
Mayank Mishra <mayank71104@gmail.com>
2024-09-09 12:11:55 +0530

    commit 2

--- Django-SalesandRetail/build.sh
+++
@@ -1,4 +0,0 @@
-set -o errexit
-pip install -r SalesandRetail/requirements.txt
-python SalesandRetail/manage.py collectstatic --no-input
-python SalesandRetail/manage.py migrate
---
+++ build.sh
@@ -0,0 +1,4 @@
+set -o errexit
+pip install -r requirements.txt
+python manage.py collectstatic --no-input
+python manage.py migrate
